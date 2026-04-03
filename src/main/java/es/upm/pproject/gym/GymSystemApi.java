84cfcd2cf2fd71cadfb23c49f0965a064ea7c5ea
FrancoSosaMartinez <franco.sosa.martinez@alumnos.upm.es>
2026-04-03 12:56:19 +0200

    GymSystemApi.java

---
+++ src/main/java/es/upm/pproject/gym/GymSystemApi.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public interface GymSystemApi {
+    void registerClass(String className, String trainer);
+
+    void registerPerson(Integer identificationNumber, String name, String email);
+
+    void enrollPersonInClass(String email, String className);
+
+    List<Person> getPersonsInClass(String className);
+
+    void cancelEnrollment(String email, String className);
+
+    void restartClass(String className);
+
+    List<Person> getAllRegisteredPeople();
+
+    List<GymClass> getAllRegisteredClasses();
+}
